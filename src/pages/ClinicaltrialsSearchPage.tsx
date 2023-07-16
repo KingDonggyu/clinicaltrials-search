039554aa461550b561746d754572faebaa802cf3
KingDonggyu <dgkim69166916@gmail.com>
2023-07-16 18:50:45 +0900

    feat(#2): Background 및 제목 텍스트 스타일링

--- src/pages/ClinicaltrialsSearchPage.tsx
+++ src/pages/ClinicaltrialsSearchPage.tsx
@@ -1,3 +1,13 @@
+import colors from 'constants/colors';
+
 export function ClinicaltrialsSearchPage() {
-  return <div>임상시험 검색 페이지</div>;
+  return (
+    <main css={{ background: colors.blue100, padding: '4em 1em' }}>
+      <h1 css={{ fontSize: '1.8em', fontWeight: 'bold', textAlign: 'center', lineHeight: 1.6 }}>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </h1>
+    </main>
+  );
 }
