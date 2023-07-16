2f720d8af794ce4a2430621b449f19183ba4f675
KingDonggyu <dgkim69166916@gmail.com>
2023-07-16 18:27:23 +0900

    feat(#1): 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from 'App';
+
+const router = createBrowserRouter([]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>
 );
