cb578d9694ba79d6a65bbcc00cc9e9f6f6f1e700
KingDonggyu <dgkim69166916@gmail.com>
2023-07-16 18:30:10 +0900

    feat(#1): Http Client 모듈 구현

---
+++ src/lib/HttpClient.ts
@@ -0,0 +1,23 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+
+class HttpClient {
+  protected axios;
+
+  constructor() {
+    this.axios = Axios.create({
+      baseURL: import.meta.env.VITE_BASE_URL,
+    });
+  }
+
+  async get<Response = unknown>(url: string, config?: AxiosRequestConfig) {
+    const res = await this.axios.get<Response>(url, config);
+    return res.data;
+  }
+
+  async post<Response = unknown, Request = any>(url: string, body?: Request) {
+    const res = await this.axios.post<Response>(url, body);
+    return res.data;
+  }
+}
+
+export default HttpClient;
