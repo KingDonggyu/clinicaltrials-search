72f1ba245af9e8acb6b7397995f9029be992585f
KingDonggyu <dgkim69166916@gmail.com>
2023-07-16 18:19:55 +0900

    feat(#1): 전역 스타일 설정

---
+++ src/styles/globals.ts
@@ -0,0 +1,49 @@
+import { css } from '@emotion/react';
+import normalize from 'emotion-normalize';
+
+export const globalStyle = css`
+  ${normalize}
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+      'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: 1em;
+    font-weight: normal;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul,
+  ol {
+    list-style: 'none';
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+`;
