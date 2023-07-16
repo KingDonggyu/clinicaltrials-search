814a664032a26835ca2ed25b84185830919ab86a
KingDonggyu <dgkim69166916@gmail.com>
2023-07-16 18:43:00 +0900

    feat(#2): 임상시험 검색 페이지 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from 'App';
 
-const router = createBrowserRouter([]);
+import App from 'App';
+import { ClinicaltrialsSearchPage } from 'pages/clinicaltrialsSearchPage';
+
+const router = createBrowserRouter([{ path: '/', element: <ClinicaltrialsSearchPage /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/pages/ClinicaltrialsSearchPage.tsx
@@ -0,0 +1,3 @@
+export function ClinicaltrialsSearchPage() {
+  return <div>임상시험 검색 페이지</div>;
+}
